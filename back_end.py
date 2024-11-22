0d7719fb381e79cf1a62694901d3d802941ce38f
paulv <paulvorderbruegge@gmail.com>
2024-11-23 00:10:31 +0100

    Added 'Event' Class

--- back_end.py
+++ back_end.py
@@ -1,5 +1,21 @@
 from flask import Flask, request
+from datetime import datetime
+import uuid
+
+event_id = str(uuid.uuid4())
 
 app = Flask(__name__)
 
-#TODO
+
+class Event:
+
+    def __init__(self, event_title, event_description, event_picture, event_date, event_location, event_host, event_visitors):
+        self.id = str(uuid.uuid4())
+        self.title = event_title
+        self.description = event_description
+        self.picture = event_picture
+        self.event_date = event_date
+        self.create_date = datetime.now()
+        self.location = event_location
+        self.host = event_host
+        self.visitors = event_visitors
