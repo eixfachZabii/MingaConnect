dec05534a27476d980c9247376ba39f450c9843d
paulv <paulvorderbruegge@gmail.com>
2024-11-23 09:22:55 +0100

    Added a update_event REST endpoint

--- back_end.py
+++ back_end.py
@@ -57,7 +57,80 @@ def create_event():
         event_list.append(new_event)
 
         # Return the event_id
-        return jsonify({"event_id": new_event.id}), 201
+        return jsonify({'event_id': new_event.id}), 201
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/update_event', methods=['PUT'])
+def update_event():
+    try:
+        # Parse JSON data from the request
+        data = request.get_json()
+
+        # Check if an id was given
+        if 'event_id' not in data:
+            return jsonify({'error': 'No event specified'}), 400
+        else:
+            for element in event_list:
+                if element.id == data.get('event_id'):
+                    event = element
+                    break
+        
+        # Update the given attributes
+        if 'title' in data:
+            event.title = data.get('title')
+        if 'description' in data:
+            event.description = data.get('description')
+        if 'picture' in data:
+            event.picture = data.get('picture')
+        if 'date' in data:
+            event.event_date = data.get('date')
+        if 'location' in data:
+            event.location = data.get('location')
+        if 'host' in data:
+            event.host = data.get('host')
+
+        # Return the event_id
+        return jsonify({'event_id': event.id}), 201
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+#delete event
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    try:
+        # Parse JSON data from the request
+        data = request.get_json()
+
+        # Set default values for attributes
+        title = data.get('title', 'Untitled Event')
+        description = data.get('description', 'No description provided')
+        picture = data.get('picture', 'default-picture-url.jpg TODO')
+        date = data.get('date', None)
+        location = data.get('location', 'No location specified')
+        host = data.get('host', 'Anonymous')
+
+        # Create a new Event object
+        new_event = Event(
+            event_title=data['title'],
+            event_description=data['description'],
+            event_picture=data['picture'],
+            event_date=data['date'],
+            event_location=data['location'],
+            event_host=data['host']
+        )
+
+        # Add the event to the event_list
+        event_list.append(new_event)
+
+        # Return the event_id
+        return jsonify({'event_id': new_event.id}), 201
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+#update user
