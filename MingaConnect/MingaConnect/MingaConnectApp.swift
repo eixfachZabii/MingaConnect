fcc5b629660fd19627cca39efbfacf07d02bd19c
Matthias Meierlohr <matthi24.m@web.de>
2024-11-23 09:16:08 +0100

    XCode Project Setup

---
+++ MingaConnect/MingaConnect/MingaConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MingaConnectApp.swift
+//  MingaConnect
+//
+//  Created by Matthias Meierlohr on 23.11.24.
+//
+
+import SwiftUI
+
+@main
+struct MingaConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
