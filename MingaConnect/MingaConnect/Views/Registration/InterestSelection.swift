702a50590c6bc625cb18b18c999fcb30827a3c0c
Matthias Meierlohr <matthi24.m@web.de>
2024-11-24 09:34:04 +0100

    jdsilfa

--- MingaConnect/MingaConnect/Views/Registration/InterestSelection.swift
+++ MingaConnect/MingaConnect/Views/Registration/InterestSelection.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct InterestSelection: View {
     @Binding var isPresented: Bool
     @State private var selectedInterests: Set<String> = []
-    let interests = ["Bouldering", "Hiking", "Pub Crawls", "Chess", "Picnics", "Museums", "Boat", "Running", "Board Games", "Meet new people"]
+    let interests = ["Bouldering", "Hiking", "Meet new people","Pub Crawls", "Chess", "Picnics", "Museums", "Boat", "Running", "Board Games"x]
     
     var body: some View {
         VStack(spacing: 20) {
